db8c960fb2d957be3cd4c9f48944eca7d779613f
Marjan18101013 <63208270+Marjan18101013@users.noreply.github.com>
2020-09-22 14:04:53 +0600

    ok

--- HelloPaw_final/urls.py
+++ HelloPaw_final/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from user import views as userviews
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', userviews)
+
 
 ]

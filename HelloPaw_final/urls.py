dfca00564f528bef09cca63f309ff5469ddfa12a
Rakin Mohammad Sifullah <18101003@uap-bd.edu>
2020-09-22 14:42:36 +0600

    home page

--- HelloPaw_final/urls.py
+++ HelloPaw_final/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_views.show_home)
 
 
 ]

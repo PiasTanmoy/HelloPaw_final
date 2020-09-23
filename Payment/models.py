467ccdc53c55923b8e7d25147a5a3ea5f257a2c6
Marjan18101013 <63208270+Marjan18101013@users.noreply.github.com>
2020-09-23 23:27:55 +0600

    fk

--- Payment/models.py
+++ Payment/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from PetService.models import Service
+from PetShop.models import Petshop
 
 
 # Create your models here.
 class Payment(models.Model):
     amount = models.CharField(max_length=100)
     type = models.CharField(max_length=50)
+
+    S_id = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
+    P_id = models.ForeignKey(Petshop, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.id
--- User/models.py
+++ User/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from PetShop.models import Petshop
+from PetService.models import Service
 # Create your models here.
 class Customer(models.Model):
     name = models.CharField(max_length=100)
@@ -9,4 +10,18 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+class CustomerProduct(models.Model):
+    c_id = models.ForeignKey(Customer, on_delete=models.SET_NULL,null=True, blank=True)
+    p_id = models.ForeignKey(Petshop, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.Customer.name + " : " + self.Petshop.name
+
+class CustomerService(models.Model):
+    c_id = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True)
+    s_id = models.ForeignKey(Service,on_delete=models.SET_NULL,null=True)
+
+    def __str__(self):
+        return self.Customer.name + " : " + self.Service.name
 

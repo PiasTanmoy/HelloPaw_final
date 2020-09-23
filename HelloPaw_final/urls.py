b491dcf18d72d35839e5811a288c932d84ed899a
ayesha163 <18101001@uap-bd.edu>
2020-09-23 19:15:42 +0600

    ayesha

--- HelloPaw_final/urls.py
+++ HelloPaw_final/urls.py
@@ -18,11 +18,12 @@ from django.urls import path
 from User import views as userviews
 from Home import views as home_views
 from Petservice import views as service_views
+from PetShop import views as product_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users',userviews.showCustomer),
     path('', home_views.show_home),
     path('about/', home_views.show_about),
     path('service/', service_views.show_service),
-
+    path('shop/',product_views.showproduct)
 ]
